aa345fe124a0ecb7b9bf2ac3c91df3418690d0d0
brockelsbya1148 <110875387+brockelsbya1148@users.noreply.github.com>
2022-10-18 14:56:46 +1300

    Add files via upload

---
+++ ZZ_modulo_play.py
@@ -0,0 +1,10 @@
+# Modulo example
+
+keep_going = ""
+while keep_going == "":
+
+    num_lollies = int(input("How many lollies? "))
+    num_students = int(input("How many students"))
+
+    # Lollies per student (divide)
+    
